b1607165b85a3ec8003d664908d641ac4e1ce142
Amir Lau <67386096+Amir-D-Shadow@users.noreply.github.com>
2021-03-21 20:39:33 +0800

    Add files via upload

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -21,6 +21,8 @@ class Logistic_Regression:
         self.dim1 = height
         self.dim2 = width
         self.dim3 = depth
+        self.grad = {}
+        self.para = {}
 
         self.Data_Filtering()
         self.construct_data_set()
@@ -38,11 +40,12 @@ class Logistic_Regression:
 
             image_data = Image.open(source_path+"/"+data)
             
-            if image_data.size[0] != 1440 or image_data.size[1] != 1080:
+            if image_data.size[0] != self.dim1 or image_data.size[1] != self.dim2:
 
-                image_data = image_data.resize((1440,1080))
+                image_data = image_data.resize((self.dim1,self.dim2))
 
             image_data.save(data_path+"/"+data)
+            
 
     def label_name(self,name):
 
@@ -63,14 +66,7 @@ class Logistic_Regression:
         data_list.remove(".DS_Store")
 
         #initialize array
-
-        path = data_path + "/" + data_list[0]
-        data_image = plt.imread(path).reshape((1,self.dim1,self.dim2,self.dim3))
-
-        self.data_X = np.array(data_image)
-        self.data_Y.append(self.label_name(data_list[0]))
-
-        #---------------------------------------------------------------------
+        self.data_X = np.zeros((1,self.dim1,self.dim2,self.dim3))
 
         for data in data_list:
             
@@ -80,13 +76,175 @@ class Logistic_Regression:
             self.data_X = np.append(self.data_X,data_image,axis = 0)
             self.data_Y.append(self.label_name(data))
 
-        self.data_X = self.data_X.reshape(self.data_X.shape[0],-1).T/255
-        self.data_Y = np.array(self.data_Y).reshape(1,len(self.data_Y))
 
+        self.data_X = np.delete(self.data_X,(0),axis = 0)
+        self.data_X = (self.data_X.reshape(self.data_X.shape[0],-1).T)/255
+        self.data_Y = np.array(self.data_Y).reshape(1,len(self.data_Y))
+        
+
+    def initialize_para_zero(self):
+
+        """
+         w : (1,num_of_px_h*num_of_px_w*depth)
+         b : scalar
+
+        """
+
+        w = np.zeros((1,self.dim1*self.dim2*self.dim3))
+        b = 0
+
+        return w,b
+
+
+    def sigmoid(self,z):
+
+        sig = 1/(np.exp(-z)+1) #<- overflow [sometime )
+        #sig = 1/(scipy.special.expit(-z)+1)
+        #sig = 1/(np.tanh(-z*0.000000001)+1)
+        
+
+
+        return sig
+
+
+    def propagate(self,w,b,X,Y):
+
+        """
+        w : (1,num_of_px_h*num_of_px_w*depth)
+        b : scalar
+        X : (num_of_px_h*num_of_px_w*depth,num_of_samples)
+        Y : (1,num_of_samples)
+        Z : (1,num_of_samples)
+        """
+
+        m = X.shape[1]
+        
+        #Forward Propagation
+        Z = np.dot(w,X) + b
+        A = self.sigmoid(Z)
+        cost = np.sum((-Y*np.log(A)-(1-Y)*np.log(1-A)))/m
+
+        #Backward Propagation
+        dw = np.dot(X,(A-Y).T)/m
+        db = np.sum((A-Y))/m
+
+        #Summmarize
+        self.grad["dw"] = dw.T
+        self.grad["db"] = db
+        
+        return cost
+
+    
+
+    def Optimization(self,w,b,X,Y,Iteration = 2000,alpha = 0.01,print_cost = False):
+
+
+        """
+        w : (1,num_of_px_h*num_of_px_w*depth)
+        b : scalar
+        X : (num_of_px_h*num_of_px_w*depth,num_of_samples)
+        Y : (1,num_of_samples)
+        Z : (1,num_of_samples)
+        
+        """
+
+        for i in range(Iteration):
+
+            #For and back Propagation
+            cost = self.propagate(w,b,X,Y)
+
+            #Update parameter
+            w = w - alpha*self.grad["dw"]
+            b = b - alpha*self.grad["db"]
+
+            if print_cost and i%100 == 0:
+
+                print("Cost after iteration %i: %f" %(i, cost))
                 
 
+        #Summmarize             
+        self.para["w"] = w
+        self.para["b"] = b
 
-            
+    def Classification(self,w,b,sample):
+
+        """
+        w : (1,num_of_px_h*num_of_px_w*depth)
+        b : scalar
+        sample : (num_of_px_h*num_of_px_w*depth,num_of_samples)
+        Z : (1,num_of_samples)
+        A : (1,num_of_samples)
+        """
+
+        Z = np.dot(w,sample)+b
+        A = self.sigmoid(Z)
+
+        result = []
+        
+        for i in range(A.shape[1]):
+
+            if A[0][i] > 0.5:
+
+                result.append(1)
+
+            else:
+
+                result.append(0)
+
+
+        return np.array(result).reshape(1,len(result))
+
+    
+    def Modelling(self,X,Y,Iteration = 2000,alpha = 0.5,print_cost = False):
+        
+
+        """
+        w : (1,num_of_px_h*num_of_px_w*depth)
+        b : scalar
+        X : (num_of_px_h*num_of_px_w*depth,num_of_samples)
+        Y : (1,num_of_samples)
+        Z : (1,num_of_samples)
+        
+        """
+
+        #initialization
+        w,b = self.initialize_para_zero()
+
+        #Train Model
+        self.Optimization(w,b,X,Y,Iteration,alpha,print_cost)
+
+        #Predict Result
+        Prediction_Train = self.Classification(self.para["w"],self.para["b"],X)
+        Accuracy_Train = 100 - np.mean(np.abs(Prediction_Train - Y))*100
+
+        print("Accuracy(Training Set):{}%".format(Accuracy_Train))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
 
 
 
--- main.py
+++ main.py
@@ -14,10 +14,7 @@ from scipy import ndimage
 my_image = "y_66.jpg"
 path = os.getcwd()
 
-
 fname = path + "/Source/" + my_image
-
-
 
 #They are the same
 image = np.array(plt.imread(fname))
@@ -28,16 +25,17 @@ image_test = np.array(Image.open(fname))
 image_1 = plt.imread(fname)
 image_2 = Image.open(fname)
 
-#They are different
+#They are different since image_1 and image_2 are of different data type
 image_1.size
 image_2.size
 
+image_3 = plt.imread(fname)
 image_2 = plt.imread(fname)
-image_2 = image_2.reshape((image_2.shape[0],image_2.shape[1],image_2.shape[2],1))
-image_1 = image_1.reshape((image_1.shape[0],image_1.shape[1],image_1.shape[2],1))
-data_set = np.empty((2884,720,3,1))
-data_set = np.append(data_set,image_1,axis = 3)
-data_set = np.append(data_set,image_2,axis = 3)
+image_2 = image_2.reshape((1,image_2.shape[0],image_2.shape[1],image_2.shape[2]))
+image_1 = image_1.reshape((1,image_1.shape[0],image_1.shape[1],image_1.shape[2]))
+data_set = np.empty((1,2884,720,3))
+data_set = np.append(data_set,image_1,axis = 0)
+data_set = np.append(data_set,image_2,axis = 0)
 
 
 #data_set = np.array(data_set)
@@ -49,11 +47,41 @@ test_image = test_image.resize((1440,1080))
 #test_image.save(os.getcwd()+"/Data/"+"y_2.jpg")
 
 #They are the same
-plt.imshow(plt.imread(fname))
-plt.imshow(image)
+#plt.imshow(plt.imread(fname))
+#plt.imshow(image)
 #-------------------
 #plt.show()
 
+"""
+w, b, X, Y = np.array([[1.],[2.]]).reshape(1,2), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]]).reshape(1,3)
+a = LR.Logistic_Regression(data_file_name = "Data",source_file_name = "Source")
+
+cost_1 = a.propagate(w,b,X,Y)
+list_r = a.Classification(w,b,X)
+
+a.Optimization(w, b, X, Y, 100, 0.009, print_cost = False)
+print ("w = " + str(a.para["w"]))
+print ("b = " + str(a.para["b"]))
+print ("dw = " + str(a.grad["dw"]))
+print ("db = " + str(a.grad["db"]))
+
+w,b =a.para["w"],a.para["b"]
+cost_2 = a.propagate(w,b,X,Y)
+
+"""
+
+
 #-----------------------------------------------------------------------------
 
-a = LR.Logistic_Regression(data_file_name = "Data",source_file_name = "Source")
+"""
+
+Test = LR.Logistic_Regression(height=1440,width = 1080,data_file_name = "Data",source_file_name = "Source")
+
+Test.Modelling(Test.data_X,Test.data_Y,print_cost = True)
+
+"""
+
+#np.savetxt(path+"/"+"Data_X.csv",Test.data_X,delimiter = ',',fmt='%f')
+#np.savetxt(path+"/"+"Data_Y.csv",Test.data_Y,delimiter = ',',fmt='%d')
+
+
