a23101b4fb9e5bb6d6bbafd327828de6f07b066d
Amir Lau <67386096+Amir-D-Shadow@users.noreply.github.com>
2021-03-23 00:55:50 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ image_1 = plt.imread(fname)
 image_2 = Image.open(fname)
 
 #They are different since image_1 and image_2 are of different data type
-image_1.size
-image_2.size
+print(type(image_1))
+print(type(image_2))
 
+print(image_1.shape)
+print(image_2.size)
+"""
 image_3 = plt.imread(fname)
 image_2 = plt.imread(fname)
 image_2 = image_2.reshape((1,image_2.shape[0],image_2.shape[1],image_2.shape[2]))
@@ -36,7 +39,7 @@ image_1 = image_1.reshape((1,image_1.shape[0],image_1.shape[1],image_1.shape[2])
 data_set = np.empty((1,2884,720,3))
 data_set = np.append(data_set,image_1,axis = 0)
 data_set = np.append(data_set,image_2,axis = 0)
-
+"""
 
 #data_set = np.array(data_set)
 
@@ -54,7 +57,7 @@ test_image = test_image.resize((1440,1080))
 
 """
 w, b, X, Y = np.array([[1.],[2.]]).reshape(1,2), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]]).reshape(1,3)
-a = LR.Logistic_Regression(data_file_name = "Data",source_file_name = "Source")
+a = LR.Logistic_Regression(data_file_name = "Data",source_file_name = "Test")
 
 cost_1 = a.propagate(w,b,X,Y)
 list_r = a.Classification(w,b,X)
@@ -83,6 +86,7 @@ print(b[0,2])
 
 #-----------------------------------------------------------------------------
 
+#Linearly Separable ?
 """
 
 Test = LR.Logistic_Regression(height=64,width = 64,data_file_name = "Data",source_file_name = "Source")
